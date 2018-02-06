d6a24298e4160e46fd979887585c7b7e86355e4e
Diego Artusio <d.artusio@pragmos.it>
2018-02-06 10:10:06 +0100

    Add project files.

---
+++ SimplyUpdate.BuildUpdate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimplyUpdate.BuildUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
